a85331f2ba0e848137eb1633ca8f693f8fe6ab7e
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-02-02 09:08:33 -0500

    fix mineur

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,38 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
-  filename: 'index.html',
-  inject: 'body'
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
 });
 
 module.exports = {
     entry: [
-    './app/index.js'
+        './app/index.js'
     ],
     output: {
-    path: __dirname + '/dist',
-    filename: "index_bundle.js"
+        path: __dirname + '/dist',
+        filename: "index_bundle.js"
     },
 
     plugins:[
     new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
+        'process.env':{
+            'NODE_ENV': JSON.stringify('production')
+        }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
-      }
+        compress:{
+            warnings: true
+        }
     }),
     HTMLWebpackPluginConfig
     ],
 
     module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-      {test: /\.css$/, loader: "style-loader!css-loader"}
-    ]
+        loaders: [
+          {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+          {test: /\.css$/, loader: "style-loader!css-loader"}
+        ]
     },
 };
