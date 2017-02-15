cf56a5e35d43bdabb8f1b6fc2d34d68579bd5097
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-02-15 17:52:20 -0500

    tentative de fix pour le vh en mobile

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -12,11 +12,25 @@ export default class Portfolio extends Component {
         });
     }
 
+    fixVH() {
+        function calcVH() {
+            let vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+            let list = document.getElementsByClassName("projet")
+            for (let i = 0; i < list.length; i++){
+                list[i].setAttribute(" style ", " height : " + vH + " px;");
+            }
+        }
+        calcVH();
+        window.addEventListener('resize', calcVH, true);
+        window.addEventListener('onorientationchange', calcVH, true);
+    }
+
     render() {
         return (
             <section id="sectionProjet">
                 <h2>{this.props.projets.titre}</h2>
                 { this.renderProjets() }
+                {this.fixVH()}
             </section>
         )
     }
--- app/components/Projets.js
+++ app/components/Projets.js
@@ -3,17 +3,17 @@ import React, {Component} from "react";
 const iconGit = require('../icons/github.png');
 const iconLink = require('../icons/link.png');
 
-export default class Projets extends Component{
+export default class Projets extends Component {
 
-    render(){
+    render() {
         return (
             <article className={["projet", this.props.projet.classe].join(' ')}>
                 <div>
                     <h3>{this.props.projet.titre}</h3>
                     <p>{this.props.projet.description}</p>
                     <div>
-                        <a href={this.props.projet.lien} alt="lien vers le projet"><img className="icon" src={iconLink} /></a>
-                        <a href={this.props.projet.github} alt="lien vers le github du projet"><img className="icon" src={iconGit} /></a>
+                        <a href={this.props.projet.lien} alt="lien vers le projet"><img className="icon" src={iconLink}/></a>
+                        <a href={this.props.projet.github} alt="lien vers le github du projet"><img className="icon" src={iconGit}/></a>
                     </div>
                 </div>
             </article>
