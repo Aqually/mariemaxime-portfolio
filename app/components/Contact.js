c76ea018950ff3b82edad3b11f4681f7590da3d1
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-03-01 09:29:09 -0500

    test du serveur email

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -2,17 +2,39 @@
 
 import React, {Component} from "react";
 
-export default class Contact extends Component{
-    render(){
+export default class Contact extends Component {
+    constructor() {
+        super();
+        this.onSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var self = this;
+        fetch('./contact', {
+            method: 'POST',
+            data: {
+                nom: self.refs.nom,
+                email: self.refs.email,
+                message: self.refs.message
+            }
+        }).then(function(response) {
+            return response.json()
+        }).then(function(body) {
+            console.log(body);
+        });
+    }
+
+    render() {
         return (
             <section id="contact">
                 <h2>{this.props.contact.titre}</h2>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div>
-                        <input type="text" id="name" name="user_name" placeholder={this.props.contact.nom} />
-                        <input type="email" id="mail" name="user_mail" placeholder={this.props.contact.couriel} />
+                        <input type="text" id="name" ref="nom" name="user_name" placeholder={this.props.contact.nom}/>
+                        <input type="email" id="mail" ref="email" name="user_mail" placeholder={this.props.contact.couriel}/>
                     </div>
-                    <textarea id="msg" name="user_message" placeholder={this.props.contact.message}></textarea>
+                    <textarea id="msg" name="user_message" ref="message" placeholder={this.props.contact.message}></textarea>
                     <button type="submit">Envoyer</button>
                 </form>
             </section>
--- server.js
+++ server.js
@@ -2,12 +2,43 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
+const nodemailer = require('nodemailer');
 
 app.use(express.static(__dirname + "/public"));
-
 app.use(express.static(__dirname + "/dist"));
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/dist", "index.html"));
 })
+
+
+// create reusable transporter object using the default SMTP transport
+var smtpTransport = nodemailer.createTransport("SMTP",{
+   service: "Gmail",  // sets automatically host, port and connection security settings
+   auth: {
+       user: "myemail@gmail.com",
+       pass: "mypass"
+   }
+});
+
+app.post('/contact', function(req, res) {
+
+    console.log(req.body);
+    var data = req.body;
+
+    smtpTransport.sendMail({
+        from: "Sender Name <sender@gmail.com>",
+        to: "Receiver Name <reciver@gmail.com>",
+        subject: "Confirmation Mail",
+        text: "Messege From " + data.name
+    }, function(error, response) { //callback
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(" Message sent");
+        }
+        smtpTransport.close(); // shut down the connection pool, no more messages. Comment this line out to continue sending emails.
+    });
+    res.json(data);
+});
 
 app.listen(port);
