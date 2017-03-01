c99f70cb1f520e3cea30fb0f0ced6f967a3c2bb7
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-03-01 10:52:15 -0500

    test du serveur email

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ let mailOptions = {
 
 app.post('/contact', function(req, res) {
     var data = req.body;
-    console.log(req.body);
+    console.log(req, res);
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             return console.log(error);
