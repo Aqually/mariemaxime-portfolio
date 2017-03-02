b6f8de1fd322936c2e929d4bd6ac52bc1204f81f
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-03-01 19:13:05 -0500

    test du serveur email

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import PortFolioReducer from "./reducer_portfolio";
-import SendEmailReducer from "./reducer_email";
 
 const rootReducer = combineReducers({
     portfolio: PortFolioReducer
