b332e2e8814e98e961992c1a4bcd5a558bce3609
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-03-02 20:03:06 -0500

    css de a propos

--- app/components/Propos.js
+++ app/components/Propos.js
@@ -6,7 +6,7 @@ export default class Propos extends Component {
         return (
             <section id="propos">
                 <h2>À propos de moi</h2>
-                <article>
+                <article className="propos_article">
                     <p>Je suis Marie-Maxime Tanguay, étudiante en multimédia qui adore la programmation et l’intégration Web et qui désire une carrière dans ce domaine.</p>
                     <p>Amatrice de jeux vidéo, de technologies, de films de super héros et de patin à roues alignées.</p>
                 </article>
