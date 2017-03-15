516529067369ee761f31c469612047aafa3ff4d3
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-03-15 11:39:29 -0400

    ajout de l'image d'intro à firebase storage

--- app/components/Description.js
+++ app/components/Description.js
@@ -2,14 +2,11 @@
 
 import React, {Component} from "react";
 
-const imgMarie = require('../img/marie-maxime-tanguay.png');
-
-
 export default class Description extends Component{
     render(){
         return (
             <article>
-                <img src={imgMarie} alt={this.props.image.alt} title={this.props.image.alt} />
+                <img src={this.props.image.url} alt={this.props.image.alt} title={this.props.image.alt} />
                 <p>{this.props.description}</p>
             </article>
         )
